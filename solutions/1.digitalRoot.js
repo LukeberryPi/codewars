b7d8e1fd5fc1f98447109fc9270dc010ed813185
Luke Berry <lukeberrypi@gmail.com>
2023-01-25 16:57:41 -0300

    improve digitalRoot solution

---
+++ solutions/1.digitalRoot.js
@@ -0,0 +1,14 @@
+const EXERCISE_LINK = "https://www.codewars.com/kata/541c8630095125aba6000c00";
+
+const sampleInput = 38;
+
+function digitalRoot(n) {
+  if (n < 10) return n;
+
+  const split = String(n).split('').map(e => Number(e));
+  const sum = split.reduce((a, b) => a + b);
+
+  return digitalRoot(sum);
+}
+
+console.log(digitalRoot(sampleInput));
