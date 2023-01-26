e9ddff48faa14879c8526efd0dc431493953b266
Luke Berry <lukeberrypi@gmail.com>
2023-01-26 18:29:08 -0300

    improve digitalRoot solution

--- solutions/1.digitalRoot.js
+++ solutions/1.digitalRoot.js
@@ -1,12 +1,16 @@
 const EXERCISE_LINK = "https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript";
 
-const sampleInput = 38;
+const sampleInput = 942;
+
+// somar todos os algarismos do número
+// caso o número seja menor que 10, retorne o número
+// caso não seja, some os algarismos do número produzido novamente
 
 function digitalRoot(n) {
   if (n < 10) return n;
 
-  const split = String(n).split('').map(e => Number(e));
-  const sum = split.reduce((a, b) => a + b);
+  const split = String(n).split('').map(e => parseInt(e));
+  const sum = split.reduce((acc, cur) => acc + cur);
 
   return digitalRoot(sum);
 }
