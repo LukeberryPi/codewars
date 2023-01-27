fc16952c92deff5a26b3637693df3d2384987f58
Luke Berry <lukeberrypi@gmail.com>
2023-01-27 15:18:47 -0300

    improve narcissistic solution

--- solutions/3.narcissistic.js
+++ solutions/3.narcissistic.js
@@ -1,18 +1,27 @@
 const EXERCISE_LINK = "https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/javascript";
 
-const sampleInput = 371;
+const sampleInput = 153;
+
+// número narcisista é um número em que a soma de todos
+// os seus dígitos individualmente elevados ao comprimento
+// do número é igual ao próprio número
+// recebemos um número e devemos retornar uma bool true or false
+
+// 7 => 7^1 => 7 => true (é narcisista)
+// 21 => 2^2 + 1^2 => 5 => false (não é narcisista)
+// 371 => 3^3 + 7^3 + 1^3 => 371 => true (é narcisista)
 
 function narcissistic(n) {
   const length = n.toString().length;
-  const digits = n.toString().split('').map(e => parseInt(e));
+  const digits = n.toString().split('').map(digit => parseInt(digit))
 
   let sumOfPoweredDigits = 0;
 
-  for (let digit of digits) {
+  digits.forEach(digit => {
     sumOfPoweredDigits += digit ** length
-  }
+  })
 
-  return sumOfPoweredDigits === n;
+  return n === sumOfPoweredDigits;
 }
 
 console.log(narcissistic(sampleInput));
