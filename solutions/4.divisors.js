bae1ef35d2d6b4b56dcbbffc701fa3293f090dc1
Luke Berry <lukeberrypi@gmail.com>
2023-01-27 17:11:42 -0300

    improve divisors solution

--- solutions/4.divisors.js
+++ solutions/4.divisors.js
@@ -1,20 +1,20 @@
 const EXERCISE_LINK = "https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/javascript";
 
-const sampleInput = 12;
+const sampleInput = 13;
+
+// recebemos um número positivo
+// temos que retornar uma lista com todos seus divisores
+// excluindo o próprio número e o 1
+// caso essa lista seja vazia, retornaremos que o número é primo
 
 function divisors(n) {
   let divisors = [];
-  let currentDivisor = n - 1;
   
-  while (currentDivisor > 1) {
-    if (n % currentDivisor === 0) {
-      divisors.push(currentDivisor);
-    }
-
-    currentDivisor--;
+  for (let d = 2; d < n; d++) {
+    if (n % d === 0) divisors.push(d);
   }
 
-  if (divisors.length) return divisors.reverse();
+  if (divisors.length) return divisors;
 
   return `${n} is prime`
 }
